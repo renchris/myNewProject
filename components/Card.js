b663f2c73f2483962ffe96a5db65b1d9cc5f946b
Chris <u4y0b@ugrad.cs.ubc.ca>
2020-08-06 00:21:55 -0700

    add additional view for Card to seperate shadow and card styling to properly render shadows for both iOS and android

--- components/Card.js
+++ components/Card.js
@@ -1,34 +1,56 @@
 import React from "react";
 import { View, StyleSheet, Text } from "react-native";
 import { Image } from "react-native-expo-image-cache";
+import { Platform } from "react-native";
 
 import colors from "../config/colors";
 
 function Card({ title, subTitle, imageURL }) {
   return (
-    <View style={styles.card}>
-      <Image style={styles.image} uri={imageURL} />
-      <View style={styles.detailsContainer}>
-        <Text style={styles.title}>{title}</Text>
+    <View style={styles.shadow}>
+      <View style={styles.card}>
+        <Image style={styles.image} uri={imageURL} />
+        <View style={styles.detailsContainer}>
+          <Text style={styles.title}>{title}</Text>
+        </View>
+        <Text style={styles.subTitle}>{subTitle}</Text>
       </View>
-      <Text style={styles.subTitle}>{subTitle}</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  shadow: {
+    ...Platform.select({
+      android: {
+        overflow: "hidden",
+      },
+      ios: {
+        overflow: "visible",
+        shadowColor: "#000",
+        shadowOffset: {
+          width: 0,
+          height: 1,
+        },
+        shadowRadius: 1,
+        shadowOffset: { width: 0, height: 0 },
+        shadowColor: "black",
+        shadowOpacity: 0.35,
+        shadowRadius: 10,
+      },
+    }),
+  },
   card: {
     borderRadius: 15,
     backgroundColor: colors.white,
-    marginHorizontal: 20,
+    margin: 25,
     overflow: "hidden",
     borderWidth: 5,
     borderColor: colors.primary,
-    shadowOffset: { width: 10, height: 50 },
     shadowColor: "black",
     shadowOpacity: 1,
     shadowRadius: 10,
-    elevation: 20,
+    elevation: 10,
     width: 200,
     justifyContent: "center",
     alignItems: "center",
